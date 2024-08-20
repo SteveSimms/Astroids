0f0bcbdf5b6d839976e3a6797855e505e05ed749
Steven Simms <simmsstev@gmail.com>
2024-08-19 22:36:01 -0700

    setting delta time and fps to 60 with the help of my 5 year old Malik

--- main.py
+++ main.py
@@ -14,17 +14,24 @@ def main():
 
     def __init__(self, title):
         pygame.init()
+        
        
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock =  pygame.time.Clock()
+    dt = 0
+    
     while True:
      for event in pygame.event.get():
         if event.type == pygame.QUIT:
            return
         
-     print("my first game loop")
+    #  print("my first game loop")
      pygame.display.flip()
+    
+     
+     dt =  clock.tick(60) / 1000.0
+     print("dt:", dt)
    
 
 if __name__ == "__main__":
